458a9cacb61f4c07d3590aef6190eb980693c881
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2023-10-07 14:01:42 +0100

    Added meta tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import Logo1 from "../assets/images/Logo1.png";
 
 const Footer = () => {
+  const date = new Date()
   return (
     <>
-    <footer></footer>
+      <footer>
+        <img className="footer-logo"
+          src={Logo1}
+          alt="Little Lemon Restaurant"
+          width={"50"}
+          height={"auto"}
+        />
+        <span>Copyright Bas Maw @ Little Lemon Restaurant {`2023 - ${date.getFullYear() > 2023 ? date.getFullYear : ''}`}</span>
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
