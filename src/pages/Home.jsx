5dc50283141f2cc4d56ab2ac20c303cccfa3418d
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2023-10-27 10:55:48 +0100

    Home refactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,31 @@ export const Home = () => {
         &#x1F378; Welcome to the Little Lemon Restaurant &#x1F378;
       </h2>
 
-      <p>the little lemon restaurant has been serving Chicago</p>
+      <p>
+        The Little Lemon restaurant has been serving Chicago for over 40 years!
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis
+        ratione earum sint minima quisquam id, optio sit dicta nihil blanditiis.
+        Tempore ducimus eaque perferendis minima nihil magni, labore fugit
+        quibusdam? Vel, nesciunt, ipsum modi voluptatum fugiat doloremque enim
+        sint magni possimus eum est dolores. Minus, commodi placeat, aut eaque
+        similique doloribus, totam culpa blanditiis ipsam odio fugit ut
+        assumenda provident.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum maxime
+        repudiandae esse cum, nesciunt quis earum voluptates consequatur quos
+        nobis tempore, officiis blanditiis corporis? Qui rerum iusto repudiandae
+        id in?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        porro, dolore non id eius cumque et nobis repellendus nemo esse ea
+        error! Cupiditate explicabo fugit ex numquam, repellat minus totam?
+      </p>
     </>
   );
 };
